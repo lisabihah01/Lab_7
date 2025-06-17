b17c1cd0818ae1dc5727b5f69118028f94aee54a
lisabihah01 <lisabihah2001@gmail.com>
2025-06-17 20:57:42 +0800

    initial commit

---
+++ register.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Register User</title>
+</head>
+<body>
+    <h2>User Registration</h2>
+    <form method="POST" action="register_process.php">
+        Matric No: <input type="text" name="matric" required><br>
+        Name: <input type="text" name="name" required><br>
+        Password: <input type="password" name="password" required><br>
+        Role:
+        <select name="role" required>
+            <option value="student">Student</option>
+            <option value="lecturer">Lecturer</option>
+        </select><br>
+        <button type="submit">Register</button>
+    </form>
+    <link rel="stylesheet" href="style.css">
+
+</body>
+</html>
